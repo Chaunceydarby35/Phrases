9773a757eb2effff6fd82a451e62a151dbced7ee
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-19 16:34:37 +0200

    updated

--- src/components/PhrasesApp.jsx
+++ src/components/PhrasesApp.jsx
@@ -1,18 +1,50 @@
+import { useState } from "react"
 
 
 const PhrasesApp = () => {
+  const [quote, setQuote] = useState({
+    text: 'Ask not what your country can do for you; ask what you can do for your country',
+    author: 'John Kennedy',
+  })
+
+  const [favorites, setFavorites] = useState([])
+  const [showFavorites, setShowFavorites] = useState(false)
+
+
+ const fetchNewQuote = async () => {
+  const url = 'https://api.quotable.io/random'
+  const response = await fetch(url)
+  const data = await response.json()
+  setQuote({
+    text: data.content,
+    author: data.author,
+  })
+ }
+
+ const toggleFavorites = () => {
+  setShowFavorites(!showFavorites)
+}
+
+const addToFavorites = () => {
+  const isAlreadyInFavorites = favorites.some(
+    (fav) => fav.text === quote.text && fav.author === quote.author,
+  )
+
+  if (!isAlreadyInFavorites) {
+    setFavorites([...favorites, quote])
+  }
+}
+
+
   return (
     <div className="container">
         <div className="phrases-app">
             <h1 className="app-heading">Phrases</h1>
             <i className="bx bxs-heart fav-icon"></i>
             <div className="phrase">
-                <i className="bx bxs-quote-alt-left left-quote"></i>
-                <p className="quote-text">
-                    Ask not what your country can do for you; ask what you
-                    can do for your country
-                </p>
-                <p className="quote-author">John Kennedy</p>
+                <i className="bx bxs-quote-alt-left left-quote" onClick={toggleFavorites}></i>
+                <p className="quote-text">{quote.text}</p>
+                <p className="quote-author">{quote.author}</p>
                 <i className="bx bxs-quote-alt-right right-quote"></i>
             </div>
             <div className="circles">
@@ -22,8 +54,12 @@ const PhrasesApp = () => {
               <div className="circle-4"></div>
             </div>
             <div className="buttons">
-              <button className="btn btn-new">New Phrase</button>
-              <button className="btn btn-fav">Add to Favorites</button>
+              <button className="btn btn-new" onClick={fetchNewQuote}>
+                New Phrase
+                </button>
+              <button className="btn btn-fav" onClick={addToFavorites}>
+                Add to Favorites
+                </button>
             </div>
         </div>
     </div>
