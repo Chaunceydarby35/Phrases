96b1a3c0bd7098ca88248dfc1a46bbd37e08e239
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-19 15:49:15 +0200

    updated

--- src/components/PhrasesApp.jsx
+++ src/components/PhrasesApp.jsx
@@ -15,7 +15,16 @@ const PhrasesApp = () => {
                 <p className="quote-author">John Kennedy</p>
                 <i className="bx bxs-quote-alt-right right-quote"></i>
             </div>
-            <div className="circles"></div>
+            <div className="circles">
+              <div className="circle-1"></div>
+              <div className="circle-2"></div>
+              <div className="circle-3"></div>
+              <div className="circle-4"></div>
+            </div>
+            <div className="buttons">
+              <button className="btn btn-new">New Phrase</button>
+              <button className="btn btn-fav">Add to Favorites</button>
+            </div>
         </div>
     </div>
   )
