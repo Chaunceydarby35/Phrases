d8268be19d3f8786588decc184a946f08ab70fd4
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-19 17:25:45 +0200

    updated

--- src/components/PhrasesApp.jsx
+++ src/components/PhrasesApp.jsx
@@ -38,53 +38,56 @@ const addToFavorites = () => {
 
   return (
     <div className="container">
-        <div className="phrases-app">
-            <h1 className="app-heading">Phrases</h1>
-            <i className="bx bxs-heart fav-icon" onClick={toggleFavorites}></i>
-            <div className="phrase">
-                <i className="bx bxs-quote-alt-left left-quote"></i>
-                <p className="quote-text">{quote.text}</p>
-                <p className="quote-author">{quote.author}</p>
-                <i className="bx bxs-quote-alt-right right-quote"></i>
-            </div>
-            <div className="circles">
-              <div className="circle-1"></div>
-              <div className="circle-2"></div>
-              <div className="circle-3"></div>
-              <div className="circle-4"></div>
-            </div>
-            <div className="buttons">
-              <button className="btn btn-new" onClick={fetchNewQuote}>
-                New Phrase
-                </button>
-              <button className="btn btn-fav" onClick={addToFavorites}>
-                Add to Favorites
-                </button>
-            </div>
-            {showFavorites && (   
-            <div className="favorites">
-              <button className="btn-close">{toggleFavorites}
-              <i className="bx bx-x"></i>
-              </button>
-              {favorites.map((favQuote, index) =>  <div className="fav-quote" key={index}>
-                <div className="fav-quote-delete">
-                  <i className="bx bx-x-circle" onClick={() => {
+    <div className="phrases-app">
+      <h1 className="app-heading">Quote.</h1>
+      <i className="bx bxs-heart fav-icon" onClick={toggleFavorites}></i>
+      <div className="quote">
+        <i className="bx bxs-quote-alt-left left-quote"></i>
+        <p className="quote-text">{quote.text}</p>
+        <p className="quote-author">{quote.author}</p>
+        <i className="bx bxs-quote-alt-right right-quote"></i>
+      </div>
+      <div className="circles">
+        <div className="circle-1"></div>
+        <div className="circle-2"></div>
+        <div className="circle-3"></div>
+        <div className="circle-4"></div>
+      </div>
+      <div className="buttons">
+        <button className="btn btn-new" onClick={fetchNewQuote}>
+          New Quote
+        </button>
+        <button className="btn btn-fav" onClick={addToFavorites}>
+          Add to Favorites
+        </button>
+      </div>
+      {showFavorites && (
+        <div className="favorites">
+          <button className="btn-close" onClick={toggleFavorites}>
+            <i className="bx bx-x"></i>
+          </button>
+          {favorites.map((favQuote, index) => (
+            <div className="fav-quote" key={index}>
+              <div className="fav-quote-delete">
+                <i
+                  className="bx bx-x-circle"
+                  onClick={() => {
                     const updatedFavorites = favorites.filter((item, i) => i !== index)
                     setFavorites(updatedFavorites)
                   }}
-                  ></i>
-                </div>
-                <div className="fav-quote-content">
-                  <div className="fav-quote-text">{favQuote.text}</div>
-                  <div className="fav-quote-author">{favQuote.author}</div>
-                </div>
-              </div> )}
-             
+                ></i>
+              </div>
+              <div className="fav-quote-content">
+                <div className="fav-quote-text">{favQuote.text}</div>
+                <div className="fav-quote-author">{favQuote.author}</div>
+              </div>
             </div>
-          )}
+          ))}
         </div>
+      )}
     </div>
-  )
+  </div>
+)
 }
 
 export default PhrasesApp
